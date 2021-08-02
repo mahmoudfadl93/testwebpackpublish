28e6b1676e7ca48e0e88580c19af4798e95464dd
mahmoud.fadl.FLAIRSTECH <mfadl4201@gmail.com>
2021-08-02 12:37:55 +0200

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,56 @@
-var webpack = require('webpack');
-var path = require('path');
+var webpack = require("webpack");
+var path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const VENDER_LIBS = [
+  "react",
+  "lodash",
+  "redux",
+  "react-redux",
+  "react-dom",
+  "faker",
+  "react-input-range",
+  "redux-form",
+  "redux-thunk",
+];
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: "./src/index.js",
+    vendor: VENDER_LIBS,
+  },
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    path: path.join(__dirname, "dist"),
+    filename: "[name].[chunkhash].js",
+  },
+  module: {
+    rules: [
+      {
+        use: "babel-loader",
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+      {
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+        ],
+        test: /\.css$/,
+      },
+    ],
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ["vendor", "manifest"],
+    }),
+
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+    }),
+  ],
 };
